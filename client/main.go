7daf574db44b92762e1a3ee3f728a688e8e4ca33
jyami <eiji@jyami.net>
2020-10-18 19:31:22 +0900

    gorilla/httpがいまいちなのでnet/httpに変更

--- client/main.go
+++ client/main.go
@@ -1,37 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
-
-	"github.com/gorilla/http"
 )
 
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("usage: %v $URL", os.Args[0])
 	}
-	//if _, err := http.Get(os.Stdout, os.Args[1]); err != nil {
-	//	log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
-	//}
 
 	url := os.Args[1]
-	log.Printf("%s\n", url)
-	status, body, reader, err := http.DefaultClient.Get(url, nil)
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	get(client, url)
+}
+
+func get(client *http.Client, url string) {
+	resp, err := client.Get(url)
 	if err != nil {
-		log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
+		panic(err)
 	}
-
-	log.Println(status.String())
-	log.Printf("%s\n", body)
-
-	if reader != nil {
-		//defer reader.Close()
-		buffer, err := ioutil.ReadAll(reader)
-		if err != nil {
-			log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
-		}
-		log.Printf("%s\n", string(buffer))
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println(resp.Status)
+	fmt.Printf("%v\n", resp.Header)
+	fmt.Println(string(body))
 }

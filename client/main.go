c4608658e76955727c32c5256a6164e125e5524a
jyami <eiji@jyami.net>
2020-10-18 17:48:28 +0900

    client update

--- client/main.go
+++ client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -11,7 +12,26 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("usage: %v $URL", os.Args[0])
 	}
-	if _, err := http.Get(os.Stdout, os.Args[1]); err != nil {
+	//if _, err := http.Get(os.Stdout, os.Args[1]); err != nil {
+	//	log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
+	//}
+
+	url := os.Args[1]
+	log.Printf("%s\n", url)
+	status, body, reader, err := http.DefaultClient.Get(url, nil)
+	if err != nil {
 		log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
+	}
+
+	log.Println(status.String())
+	log.Printf("%s\n", body)
+
+	if reader != nil {
+		//defer reader.Close()
+		buffer, err := ioutil.ReadAll(reader)
+		if err != nil {
+			log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
+		}
+		log.Printf("%s\n", string(buffer))
 	}
 }

fe355c35e0312b8e2c36c42e6aa0c7ec66efe379
jyami <eiji@jyami.net>
2020-10-18 20:27:52 +0900

    clientからlogin,secret,logout,secretを呼び出す

--- client/main.go
+++ client/main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
 )
 
@@ -13,19 +15,43 @@ func main() {
 		log.Fatalf("usage: %v $URL", os.Args[0])
 	}
 
-	url := os.Args[1]
+	u := os.Args[1]
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
+		Jar: jar,
 	}
 
-	get(client, url)
+	err = login(client, u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = secret(client, u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = logout(client, u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = secret(client, u)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func get(client *http.Client, url string) {
-	resp, err := client.Get(url)
+func get(client *http.Client, u string) {
+	resp, err := client.Get(u)
 	if err != nil {
 		panic(err)
 	}
@@ -37,4 +63,75 @@ func get(client *http.Client, url string) {
 	fmt.Println(resp.Status)
 	fmt.Printf("%v\n", resp.Header)
 	fmt.Println(string(body))
+}
+
+func login(client *http.Client, u string) error {
+	log.Printf("login\n")
+	u = u + "/login"
+
+	resp, err := client.Get(u)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.Status)
+
+	//	defer resp.Body.Close()
+	//	body, err := ioutil.ReadAll(resp.Body)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	fmt.Println(string(body))
+
+	ur, err := url.Parse(u)
+	if err != nil {
+		return err
+	}
+	cookies := client.Jar.Cookies(ur)
+
+	cookieVal := ""
+	for _, v := range cookies {
+		fmt.Printf("%v\n", v)
+		if v.Name == "cookie-name" {
+			cookieVal = v.Value
+		}
+		fmt.Printf("%s\n", cookieVal)
+	}
+
+	return nil
+}
+
+func secret(client *http.Client, u string) error {
+	log.Printf("secret\n")
+	u = u + "/secret"
+
+	resp, err := client.Get(u)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
+
+	return nil
+}
+
+func logout(client *http.Client, u string) error {
+	log.Printf("logout\n")
+	u = u + "/logout"
+
+	resp, err := client.Get(u)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
+
+	return nil
 }
